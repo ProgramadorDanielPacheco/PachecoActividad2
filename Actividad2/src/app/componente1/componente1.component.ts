f31dfc5b15af41ab70e6e62be7ce90855eab5a65
espeon <danielpacheco842005@gmail.com>
2022-12-12 12:49:06 -0500

    Formulario

--- Actividad2/src/app/componente1/componente1.component.ts
+++ Actividad2/src/app/componente1/componente1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,18 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Componente1Component implements OnInit{
 
+  constructor(private router:Router){}
+
   numero: any;
   nombre_variable:number=0;
   suma:number=0;
   vector: string[]=[];
   suma2: number=0;
   sum=0;
+  num:number=0;
+  suma3: number=0;
+
   sueldos=[1777,1451,21560,4561,2123,7856]
   ngOnInit(): void {
+    localStorage.clear();
     this.numero=1
     this.total();
     
   }
+
+  navegacion(){
+    this.router.navigate(['web2'])
+  }
+  navegacion2(){}
 
   total(){
     let numero1:number=0;
@@ -39,5 +51,11 @@ export class Componente1Component implements OnInit{
     total3=sum*2;
     console.log(total3);
   }
+
+  almacenar(){
+    localStorage.setItem('valor_total',this.numero);
+    localStorage.setItem('mensaje','almacenado');
+  }
+ 
 
 }
--- Actividad2/src/app/componente2/componente2.component.ts
+++ Actividad2/src/app/componente2/componente2.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-componente2',
   templateUrl: './componente2.component.html',
   styleUrls: ['./componente2.component.css']
 })
-export class Componente2Component {
+export class Componente2Component implements OnInit{
+  constructor(private router:Router){}
+
+  resultado:any;
+  mensaje:any;
+  suma:number=0;
+  ngOnInit(): void {
+    this.recuperar
+  }
+  navegacion(){
+    this.router.navigate(['web1'])
+  }
+  navegacion2(){
+    this.router.navigate(['web3'])
+  }
+  recuperar(){
+    this.resultado=localStorage.getItem('valor_total');
+    this.suma=2+parseInt(this.resultado);
+    this.mensaje=localStorage.getItem('mensaje');
+  }
 
 }
